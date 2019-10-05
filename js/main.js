ba25e4117ea339bed222c40c2a46e049415f09fb
Iga Alberska <alberskaiga@gmail.com>
2019-10-05 14:57:10 +0200

    final

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var opis = document.getElementById("opis");
+var button = document.querySelector('button');
+
+object.onclick = function()
+{
+    document.getElementsByTagName("p").innerHTML="Akademia 108";
+};
